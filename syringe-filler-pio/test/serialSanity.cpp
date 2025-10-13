5cfb2c347f869e36a2a2fd11a2902575830e5781
Kyenjeh Nova <kyenjeh.nova@paintbymotors.com>
2025-10-13 18:12:29 +0200

    copy everything to main

---
+++ syringe-filler-pio/test/serialSanity.cpp
@@ -0,0 +1,17 @@
+/*
+#include <Arduino.h>
+
+const int LED = 2; // onboard LED on most DevKitC/WROOM-32
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  delay(200);
+  Serial.println("\nSerial OK @115200");
+}
+
+void loop() {
+  digitalWrite(LED, !digitalRead(LED));
+  delay(500);
+}
+*/
